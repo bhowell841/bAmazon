751201fe385c330e249787faaafeb1322097d8a8
Brendan Howell <bhowell841@gmail.com>
2019-03-06 20:23:10 -0500

    changes, callback function and stuff

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 const chalk = require("chalk");
-var Table = require("cli-table");
+var Table = require('cli-table');
 
+// look up stopping asynchonous
 var choiceArray = [];
 
 // create connection
@@ -18,51 +19,84 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id ", connection.threadId);
-    showProducts();
 
-    inquirer.prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Purchase", "Exit"]
-    }).then(function (answer) {
-        if (answer.action === "Purchase") {
-            console.log("YEAH, buy some shit!");
-            // go to the purchase function
-            buyProducts();
-        } else {
-            console.log("Get the f**** out of here!");
-            connection.end();
-        }
-    })
+
+    showProducts(function(){
+        console.log(" ");
+         inquirer.prompt({
+                name: "action",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Purchase", "Exit"]
+            }).then(function (answer) {
+                if (answer.action === "Purchase") {
+                    console.log(" ");
+                    console.log("YEAH, buy some shit!");
+                    console.log(" ");
+                    
+                    // go to the purchase function
+                    buyProducts();
+                } else {
+                    console.log(" ");
+                    console.log("Get the f**** out of here!");
+                    console.log(" ");
+                    connection.end();
+                }
+            })
+        // console.log("After Products");
+    });
 });
 
 // make this look nice
-function showProducts() {
+function showProducts(myFunc) {
+
+    var table = new Table({
+        head: ['ID', 'Item', 'Department', 'Price', 'Stock']
+        , colWidths: [10, 30, 30, 30, 30]
+    });
+    
+
     connection.query("select * from products", function (err, data) {
         if (err) throw err;
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i].id + " ", data[i].productName, " ", data[i].departmentName, " ", data[i].price, " ", data[i].quantity);
+
+            var id = data[i].id,
+                productName = data[i].productName,
+                departmentName = data[i].departmentName,
+                price = data[i].price,
+                quantity = data[i].quantity;
+
+            table.push([id, productName, departmentName, price, quantity]);
         }
-        // console.log(data);
-    })
+        console.log(table.toString());
+        myFunc();
+        // return table.toString();
+    });
 }
+
+
 
 // buy shit function
 function buyProducts() {
     inquirer.prompt([{
-        name: "buyItem",
-        type: "input",
-        message: "Enter the ID of the item you would like to purchase?",
-    }, 
-    {
-        name: "buyQuantity",
-        type: "input",
-        message: "How many would you like t0 purchase?"
-    }])
-    .then(function(data){
-        connection.query("select * from products where id=?", data.buyItem, function(err, data){
-            console.log(data);
+                name: "buyItem",
+                type: "input",
+                message: "Enter the ID of the item you would like to purchase?",
+            },
+            {
+                name: "buyQuantity",
+                type: "input",
+                message: "How many would you like to purchase?"
+            }
+        ])
+        .then(function (answer) {
+            connection.query("select productName, quantity from products where id=?", answer.buyItem, function (err, data) {
+                if(err) throw err;
+                console.log(" ");
+                console.log("answers", answer.buyQuantity); // may have to parse
+                console.log("database", data[0].quantity);
+
+                // console.log(data.buyItem.quantity);
+            })
         })
-    })
-} // end og buyProducts function
+} // end of buyProducts function
